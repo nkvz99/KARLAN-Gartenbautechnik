ef114a2847109b16ed0abcd3300d4ad02ba8dec9
David Krstevski <krstevskidavid998@gmail.com>
2025-08-24 22:47:08 +0200

    offset -100

--- karlan-gartenbautechnik/src/components/NavBar/NavBar.tsx
+++ karlan-gartenbautechnik/src/components/NavBar/NavBar.tsx
@@ -21,23 +21,23 @@ export default function Navbar() {
          <img src={logo} alt="KAR-LAN Logo" className="logo" />
         <ul className={`navbar-links ${menuOpen ? "open" : ""}`}>
           <li>
-            <ScrollLink to="top" smooth duration={500} offset={-130} onClick={() => scrollToTop()}>Homepage</ScrollLink>
+            <ScrollLink to="top" smooth duration={500} offset={-100} onClick={() => scrollToTop()}>Homepage</ScrollLink>
           </li>
           <li>
-            <ScrollLink to="services" smooth duration={500} offset={-130} onClick={() => setMenuOpen(false)}>Services</ScrollLink>
+            <ScrollLink to="services" smooth duration={500} offset={-100} onClick={() => setMenuOpen(false)}>Services</ScrollLink>
           </li>
           <li>
-            <ScrollLink to="about" smooth duration={500} offset={-130} onClick={() => setMenuOpen(false)}>About Us</ScrollLink>
+            <ScrollLink to="about" smooth duration={500} offset={-100} onClick={() => setMenuOpen(false)}>About Us</ScrollLink>
           </li>
           <li>
-            <ScrollLink to="reviews" smooth duration={500} offset={-130} onClick={() => setMenuOpen(false)}>Reviews</ScrollLink>
+            <ScrollLink to="reviews" smooth duration={500} offset={-100} onClick={() => setMenuOpen(false)}>Reviews</ScrollLink>
           </li>
           <li>
-            <ScrollLink to="contact" smooth duration={500} offset={-130} onClick={() => setMenuOpen(false)}>Contact</ScrollLink>
+            <ScrollLink to="contact" smooth duration={500} offset={-100} onClick={() => setMenuOpen(false)}>Contact</ScrollLink>
           </li>
 
           <li className="mobile-only book-consultation-mobile">
-            <ScrollLink to="book" smooth duration={500} offset={-130} onClick={() => setMenuOpen(false)}>
+            <ScrollLink to="book" smooth duration={500} offset={-100} onClick={() => setMenuOpen(false)}>
               Book Consultation
             </ScrollLink>
           </li>
